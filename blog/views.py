95817bd8681a2963d7fc3a587887612bdb985180
Sourabh Verma <sourabh.coder@gmail.com>
2014-03-23 04:28:07 +0530

    Add blog app

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from models import Post
+
+class PublishedPostMixin(object):
+    def get_queryset(self):
+        queryset = super(PublishedPostMixin, self).get_queryset()
+        return queryset.filter(published=True)
+
+
+class PostListView(PublishedPostMixin, ListView):
+    model = Post
+
+class PostDetailView(PublishedPostMixin, DetailView):
+    model = Post
--- microblog/urls.py
+++ microblog/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+
+from . import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r"^$", TemplateView.as_view(template_name='index.html'))
-    # url(r'^$', 'microblog.views.home', name='home'),
-    # url(r'^microblog/', include('microblog.foo.urls')),
-
+    url(r'^$', views.HomePageView.as_view(), name='home'),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
 )
