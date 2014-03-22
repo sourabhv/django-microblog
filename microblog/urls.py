80c2e4e6f1daf5f05a320bd908b89cb3bf7aa1db
Sourabh Verma <sourabh.coder@gmail.com>
2014-03-22 23:16:00 +0530

    Create blog app and add Post model to blog

--- microblog/urls.py
+++ microblog/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r"^$", TemplateView.as_view(template_name="index.html"))
+    url(r"^$", TemplateView.as_view(template_name='index.html'))
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
 
