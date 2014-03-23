beeb1cb1eb828641ddfd5df095272bbb0ee27b6e
Sourabh Verma <sourabh.coder@gmail.com>
2014-03-23 16:01:37 +0530

    Add static url

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -9,4 +10,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.HomePageView.as_view(), name='home'),
     url(r'^blog/', include('blog.urls', namespace='blog')),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
